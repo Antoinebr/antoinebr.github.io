5911f7af818005c26aa8cba1f7b5e778db956856
Antoine Brossault <antoinebrossault@gmail.com>
2019-05-19 11:20:33 +0100

    🔥deployement

--- perso/golf/app.js
+++ perso/golf/app.js
@@ -1,6 +1,6 @@
 const fg = require('fast-glob');
 const fs = require('fs');
 
-const entries = fg.sync(['./*/'],{ onlyDirectories: true, deep: 0 }).filter( folder => folder.includes('swing') || folder.includes('driver') | folder.includes('hybrid')  );
+const entries = fg.sync(['./*/'],{ onlyDirectories: true, deep: 0 }).filter( folder => folder.includes('swing') || folder.includes('driver') || folder.includes('hybrid')  );
     
 fs.writeFileSync(__dirname+'/folderList.json',JSON.stringify(entries));
